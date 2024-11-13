820b2d303a7c30f9a9e9aa57e18b361f970fdd7e
NimZz11 <nehemiah11pietersen@gmail.com>
2024-11-13 21:03:50 +0200

    print gameboard functionality

--- naughts-and-crosses/Naughts-and-Crosses/src/Main.java
+++ naughts-and-crosses/Naughts-and-Crosses/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Rows and columns
+        char[][] gameBoard = {
+                { ' ', '|', ' ', '|', ' ' }, // row 1
+                { '-', '+', '-', '+', '-' }, // row 2
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' }
+        };
+
+        printGameBoard(gameBoard);
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
     }
 }
